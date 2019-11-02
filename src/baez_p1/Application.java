0c73687d30f12f4e25caa007fc0805ecb94ef3e9
Johnathan Baez <scoaband@hotmail.com>
2019-11-01 20:15:04 -0400

    DuplicateRemover

--- src/baez_p1/Application.java
+++ src/baez_p1/Application.java
@@ -1,7 +1,16 @@
 package baez_p1;
 
-public abstract class Application {
-    public static void main(String[] args) {
-        DuplicateRemover counter = new DuplicateRemover();
+
+import java.io.IOException;
+
+public class Application extends DuplicateRemover {
+    public static void main(String[] arg) throws IOException {
+
+        DuplicateRemover duplicateRemover = new DuplicateRemover();
+        duplicateRemover.remove("problem1.txt");
+        duplicateRemover.write("unique_words.txt");
+
+        System.out.println("\n Duplicate Remover Operation successfully done");
     }
 }
+
--- src/baez_p1/DuplicateRemover.java
+++ src/baez_p1/DuplicateRemover.java
@@ -1,4 +1,51 @@
 package baez_p1;
 
-public class DuplicateRemover {
+import java.io.*;
+import java.util.ArrayList;
+
+class DuplicateRemover{
+
+    ArrayList<String>  uniqueWords = new ArrayList<>();
+
+    public void test(String string){
+        System.out.println(string);
+    }
+
+    public void remove(String dataFile) throws IOException {
+        try {
+
+                // Buffer and read the file line by line.
+                BufferedReader buffReader = new BufferedReader(new FileReader(dataFile));
+                String firstLine = buffReader.readLine();
+
+                while (firstLine != null) {
+                    String temp[] = firstLine.split(" ");
+                    for (String i : temp) {
+
+                        if (!uniqueWords.contains(i)) {
+                            uniqueWords.add(i);
+                        }
+                    }
+                    firstLine = buffReader.readLine();
+                }
+                buffReader.close();
+         } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    public void write(String dataFile) throws FileNotFoundException {
+
+        PrintWriter printWriter = new PrintWriter(new File(dataFile));
+        for(String writeWords: uniqueWords){
+            printWriter.println(writeWords);
+        }
+        // Testing Console output!
+        System.out.print(uniqueWords);
+        printWriter.close();
+    }
 }
+
+
+
+
+
